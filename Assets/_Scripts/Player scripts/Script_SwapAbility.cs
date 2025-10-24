3ec20a05719fa0d84dd6e153775300201f25d00f
aforesttroll <aleksander.basire@gmail.com>
2025-10-24 11:18:59 +0200

    updated the swap ability
    we can now decide which body you would swap too

--- Assets/_Scripts/Player scripts/Script_SwapAbility.cs
+++ Assets/_Scripts/Player scripts/Script_SwapAbility.cs
@@ -5,10 +5,14 @@ public class Script_SwapAbility : MonoBehaviour
 {
     private Rigidbody rb;
     private GameObject newBody;
+    private int swapNumber; // Used to identify the next body to swap to
+    public int bodyNumber;
+    private int n = 1; // nr of body you're swapping to
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); // Gets the Rigidbody component
+        swapNumber = bodyNumber + 1;
     }
 
     private void Update()
@@ -32,11 +36,18 @@ public class Script_SwapAbility : MonoBehaviour
 
     private void OnSwapBody()
     {
+        Debug.Log("Swap Body Input Received");
+
         if (gameObject.tag == "Active") //checks if the current player is in control of an active body
         {
+            Debug.Log("is an active body");
+            Debug.Log("swap number is " + swapNumber);
+            Debug.Log("n is " + n);
 
-            if (GameObject.FindWithTag("InActive") == true) //checks if there is an inactive body to swap to
+            if (GameObject.FindWithTag("InActive") == true && swapNumber == n) //checks if there is an inactive body to swap to
             {
+                Debug.Log("did the swap");
+
                 newBody = GameObject.FindWithTag("InActive"); // Finding the next body to inhabit
                 gameObject.tag = "UsedPlayer"; //sets the current body to used
 
@@ -45,10 +56,9 @@ public class Script_SwapAbility : MonoBehaviour
                 newBody.tag = "Active";
                 newBody.AddComponent<Script_SwapAbility>();
                 newBody.AddComponent<PlayerMovement>();
-
-                
+                n++;
             }
-           
+
 
 
         }
