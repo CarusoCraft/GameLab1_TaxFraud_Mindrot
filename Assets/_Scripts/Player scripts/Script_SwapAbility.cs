a4e60f9ca4b23eaac7fc1279c65e50e463112d4f
aforesttroll <aleksander.basire@gmail.com>
2025-10-31 14:52:34 +0100

    fixed the error message for the swapability

--- Assets/_Scripts/Player scripts/Script_SwapAbility.cs
+++ Assets/_Scripts/Player scripts/Script_SwapAbility.cs
@@ -5,15 +5,19 @@ public class Script_SwapAbility : MonoBehaviour
 {
     private Rigidbody rb;
     private GameObject newBody;
-    private GameObject[] allBodies;
+    [SerializeField] private GameObject[] allBodies;
     [SerializeField] private int n = 1; // nr of body you're swapping to
     private int x = 0; // iterator for allBodies array
+    private GameObject nextBody;
+    private bool canSwap = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>(); // Gets the Rigidbody component
         allBodies = GameObject.FindGameObjectsWithTag("InActive"); // Finds all inactive bodies in the scene
         n = gameObject.GetComponent<playerNumber>().bodyNumber + 1; // sets n to the next body number
+        x = 0; //resets iterator for next swap
+
     }
 
     private void Update()
@@ -33,38 +37,50 @@ public class Script_SwapAbility : MonoBehaviour
             gameObject.GetComponent<MeshRenderer>().material.color = Color.red; // sets the color of the used body to red
 
         }
+
+        if (allBodies.Length > 0)
+        { 
+            while (allBodies[x].GetComponent<playerNumber>().bodyNumber != n) //looks for the correct body to swap to
+            {
+                x++;
+
+                if (x > allBodies.Length - 1)
+                {
+                    x = 0;
+                }
+            }
+
+            if (allBodies[x].GetComponent<playerNumber>().bodyNumber == n)
+            {
+                nextBody = allBodies[x];
+                canSwap = true;
+            }
+            else
+            {
+                canSwap = false;
+            }
+        }
+
+
     }
 
     private void OnSwapBody()
     {
         if (gameObject.tag == "Active") //checks if the current player is in control of an active body
         {
-            Debug.Log("n is " + n);
 
-            while (allBodies[x].GetComponent<playerNumber>().bodyNumber != n) //looks for the correct body to swap to
+
+            if(canSwap == true)
             {
-                x++;
-                Debug.Log("x is " + x);
-
-            }
-
-            if(allBodies[x].GetComponent<playerNumber>().bodyNumber == n)
-            {
-                Debug.Log("did the swap");
 
                 gameObject.tag = "UsedPlayer"; //sets the current body to used
 
 
 
                 // Inhabits the new body
-                allBodies[x].tag = "Active";
-                allBodies[x].AddComponent<PlayerMovement>();
-                allBodies[x].AddComponent<Script_SwapAbility>();
-
-            }
-            else
-            {
-                Debug.Log("didn't swap");
+                nextBody.tag = "Active";
+                nextBody.AddComponent<PlayerMovement>();
+                nextBody.AddComponent<Script_SwapAbility>();
             }
 
         }
