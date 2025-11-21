cc1536de36767ee5ec353a825bd514bbf2f3ce7b
aforesttroll <aleksander.basire@gmail.com>
2025-11-21 16:05:49 +0100

    removed unneccesary variables form EliasButton

--- Assets/_Scripts/Elias/EliasDoor.cs
+++ Assets/_Scripts/Elias/EliasDoor.cs
@@ -2,15 +2,6 @@ using UnityEngine;
 
 public class EliasDoor : MonoBehaviour
 {
-    //Speed that the door opens/closes at
-    private float speed = 1f;
-
-    
-
-    //Where the door will move open and close / down and up
-    [SerializeField] private Transform targetDown;
-    [SerializeField] private Transform targetUp;
-    
     [SerializeField] private AudioSource openSound;
     [SerializeField] private AudioSource closeSound;
 
