1f32d03f10845e75f58599cebe52295f1453f536
aforesttroll <aleksander.basire@gmail.com>
2025-12-03 23:14:46 +0100

    Fixed the sound on the timed button and added some lazers to scene one

--- Assets/_Scripts/Timed_Button.cs
+++ Assets/_Scripts/Timed_Button.cs
@@ -7,27 +7,35 @@ public class Timed_Button : MonoBehaviour
     [SerializeField][Range(5, 25)] private float timeBeforeClose;
 
     [SerializeField] private bool isPressed = false;
+    private bool doorOpen = false;
+
 
 
     [Header("Audio")]
-    private bool playedSlowSound = false;
-    private bool playedFastSound = false;
+    [SerializeField] private bool playedSlowSound = false;
+    [SerializeField] private bool playedFastSound = false;
     [SerializeField] private AudioSource tickingFast;
     [SerializeField] private AudioSource tickingSlow;
 
 
     private void Update()
     {
+
         if (isPressed == true)
         {
             buttonTimer += Time.deltaTime;
+
+            if (playedSlowSound == true)
+            {
+                Debug.Log(buttonTimer - timeBeforeClose);
+            }
 
             if (playedSlowSound == false)
             {
                 tickingSlow.Play();
                 playedSlowSound = true;
             }
-            else if (playedFastSound == false && buttonTimer >= 3)
+            else if (playedFastSound == false && timeBeforeClose - buttonTimer <= 3f )
             {
                 tickingFast.Play();
                 tickingSlow.mute = true;
@@ -39,7 +47,9 @@ public class Timed_Button : MonoBehaviour
             {
                 isPressed = false;
                 playedSlowSound = false;
+                tickingSlow.mute = false;
                 playedFastSound = false;
+                doorOpen = false;
                 tickingFast.Stop();
                 tickingSlow.Stop();
                 door.DoorClose();
@@ -53,6 +63,10 @@ public class Timed_Button : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         isPressed = true;
-        door.DoorOpen();
+        if (doorOpen == false)
+        {
+            door.DoorOpen();
+            doorOpen = true;
+        }
     }
 }
