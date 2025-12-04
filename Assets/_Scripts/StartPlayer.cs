dd34a083c07abfe5ad0b8a28521456409bc5f6a4
L-ias <elias.brattfjord.danielsen@gmail.com>
2025-12-04 17:22:59 +0100

    Made Tutorial UI for level 1

--- Assets/_Scripts/StartPlayer.cs
+++ Assets/_Scripts/StartPlayer.cs
@@ -2,46 +2,84 @@ using UnityEngine;
 
 public class StartPlayer : MonoBehaviour
 {
-    public GameObject Player_Active;
-    public GameObject MovementTutorial;
 
-    private float startCooldown = 2f;
+    public GameObject MovementTutorial;
+    public GameObject ButtonTutorial;
+    public GameObject SwapTutorial;
+
+
+    private float startCooldown = 4;
+    private float buttonCooldown = 1;
+    private float swapCooldown = 3;
 
     private bool startPlay = false;
 
-    private void Awake()
-    {
-        MovementTutorial.SetActive(true);
-    }
-
     private void Start()
     {
-        Instantiate(Player_Active.transform); //Needs to spawn at -10, 8, -20
-        Player_Active.SetActive(true);
+        MovementTutorial.SetActive(true);
+        
     }
 
 
     private void FixedUpdate()
     {
 
-        if (startPlay == false)
-        startCooldown -= Time.deltaTime;
-
-
-        if(startCooldown == 0)
+        if (startCooldown > 0)
         {
+            startCooldown -= Time.deltaTime;
+            
+        }
+        else if (startCooldown < 0)
+        {
+            startCooldown = 0;
             startPlay = true;
+            Debug.Log("Start to play.");
+            MoveTutorial();
+            buttonCooldown -= Time.deltaTime;
         }
 
+        if (buttonCooldown > 0 && startCooldown == 0)
+        {
+            buttonCooldown -= Time.deltaTime;
+        }
+        else if (buttonCooldown < 0)
+        {
+            buttonCooldown = 0;
+            ButtonTutorial.SetActive(true);
+        }
 
-        
+        if ( buttonCooldown == 0 && swapCooldown > 0)
+        {
+            swapCooldown -= Time.deltaTime;
+        }
+        else if(swapCooldown < 0)
+        {
+            swapCooldown = 0;
+        }
     }
 
 
-    private void InstatiatePlayer()
+    private void OnTriggerStay(Collider other)
     {
-        Player_Active.SetActive(true);
+        if (other.gameObject.name == "Player" && swapCooldown == 0)
+        {
+            ButtonTutorial.SetActive(false);
+            SwapTutorial.SetActive(true );
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SwapTutorial.SetActive(false );
+        }
+    }
+
+
+    private void MoveTutorial()
+    {
+        MovementTutorial.SetActive(false);
     }
 
 }
